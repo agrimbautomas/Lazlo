8615e954b9d253c2f6ce9f055deaa53d7001d52f
Tomas Agrimbau <tomas@theamalgama.com>
2016-05-12 15:31:37 -0300

    Edit js assets order and files

---
+++ app/assets/javascripts/audio-functions.js
@@ -0,0 +1,69 @@
+
+$(document).ready(function () {
+
+    papin();
+    foca();
+    siranush();
+    salchipapa();
+
+});
+
+function papin() {
+    var keys = [];
+    var konami = '80,65,80,73,78';
+
+    $(document).keydown(function (e) {
+
+        keys.push(e.keyCode);
+        if (keys.toString().indexOf(konami) >= 0) {
+            playAudio();
+            keys = [];
+        }
+    });
+}
+
+function foca() {
+    var keys = [];
+    var konami = '70,79,67,65';
+
+    $(document).keydown(function (e) {
+
+        keys.push(e.keyCode);
+        if (keys.toString().indexOf(konami) >= 0) {
+            playAudio();
+            keys = [];
+        }
+    });
+}
+
+function siranush() {
+    var keys = [];
+    var konami = '83, 73, 82, 65, 78, 85, 83, 72';
+
+    $(document).keydown(function (e) {
+
+        keys.push(e.keyCode);
+        if (keys.toString().indexOf(konami) >= 0) {
+            playAudio();
+            keys = [];
+        }
+    });
+}
+
+function salchipapa() {
+    var keys = [];
+    var konami = '83,65,76,67,72,73,80,65,80,65';
+
+    $(document).keydown(function (e) {
+
+        keys.push(e.keyCode);
+        if (keys.toString().indexOf(konami) >= 0) {
+            playAudio();
+            keys = [];
+        }
+    });
+}
+
+function playAudio() {
+    document.getElementById('audio-up').play();
+}
--- app/assets/javascripts/home-functions.js
+++ app/assets/javascripts/home-functions.js
@@ -8,11 +8,6 @@ $(document).ready(function () {
     setupResize();
     adjustStyles();
 
-    papin();
-    foca();
-    siranush();
-    salchipapa();
-
 });
 
 function setupPager() {
@@ -100,64 +95,3 @@ window.addEventListener("load", function () {
         window.scrollTo(0, 1);
     }, 0);
 });
-
-
-function papin() {
-    var keys = [];
-    var konami = '80,65,80,73,78';
-
-    $(document).keydown(function (e) {
-
-        keys.push(e.keyCode);
-        if (keys.toString().indexOf(konami) >= 0) {
-            playAudio();
-            keys = [];
-        }
-    });
-}
-
-function foca() {
-    var keys = [];
-    var konami = '70,79,67,65';
-
-    $(document).keydown(function (e) {
-
-        keys.push(e.keyCode);
-        if (keys.toString().indexOf(konami) >= 0) {
-            playAudio();
-            keys = [];
-        }
-    });
-}
-
-function siranush() {
-    var keys = [];
-    var konami = '83, 73, 82, 65, 78, 85, 83, 72';
-
-    $(document).keydown(function (e) {
-
-        keys.push(e.keyCode);
-        if (keys.toString().indexOf(konami) >= 0) {
-            playAudio();
-            keys = [];
-        }
-    });
-}
-
-function salchipapa() {
-    var keys = [];
-    var konami = '83,65,76,67,72,73,80,65,80,65';
-
-    $(document).keydown(function (e) {
-
-        keys.push(e.keyCode);
-        if (keys.toString().indexOf(konami) >= 0) {
-            playAudio();
-            keys = [];
-        }
-    });
-}
-
-function playAudio() {
-    document.getElementById('audio-up').play();
-}
