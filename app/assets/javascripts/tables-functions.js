c9503563dfd920ff96ff7b996573f89e4af0f377
Tomas Agrimbau <tomas@theamalgama.com>
2016-05-16 19:14:05 -0300

    Add js custom functions for boxes

--- app/assets/javascripts/tables-functions.js
+++ app/assets/javascripts/tables-functions.js
@@ -1,15 +1,105 @@
+var $tablesContainer;
+var containerWidth;
+var smallBoxesWidth;
+var bigBoxesWidth;
+
+var counter = 0;
+var topPointer = 0;
+var leftPointer = 0;
+var goToTop = false;
 
 $(document).ready(function () {
-    console.log('Tables!');
 
-    $(".tables-box-container").mason({
-        itemSelector: ".table-box",
-        ratio: 1.5,
-        sizes: [
-            [1,1],
-            [1,2],
-            [2,2]
-        ]
+    setupResponsiveBoxes();
+
+});
+
+function setupResponsiveBoxes() {
+    $tablesContainer = $('.tables-box-container .inner-container');
+    containerWidth = $tablesContainer.width();
+
+    setSmallBoxes();
+    setBigBoxes();
+
+    setPositions();
+}
+
+function setSmallBoxes() {
+    smallBoxesWidth = containerWidth / 5;
+    $('.small-box').css({
+        width: smallBoxesWidth,
+        height: smallBoxesWidth
+
     });
+}
+
+function setBigBoxes() {
+    bigBoxesWidth = smallBoxesWidth * 2;
+    $('.big-box').css({
+        width: bigBoxesWidth,
+        height: bigBoxesWidth
+
+    });
+}
+
+function setPositions() {
+
+    counter = 0;
+    goToTop = false;
+    topPointer = 0;
+    leftPointer = 0;
+
+
+    $('.table-box').each(function () {
+        counter++;
+
+
+
+        if (counter % 5 == 0) {
+            $(this).setBigBox();
+        } else {
+            $(this).setSmallBox();
+        }
+
+
+        if ( (counter % 2 == 0) && goToTop ) {
+            leftPointer += smallBoxesWidth;
+        }
+
+        topPointer = (goToTop) ? 0 : (topPointer + smallBoxesWidth);
+
+    });
+}
+
+
+jQuery.fn.extend({
+    setSmallBox: function () {
+        $(this).animate({
+            top: topPointer,
+            left: leftPointer
+        });
+
+        goToTop = (counter % 2 == 0) && !goToTop ? true : false;
+        
+    }
+});
+
+jQuery.fn.extend({
+    setBigBox: function () {
+        $(this).css({
+            width: bigBoxesWidth,
+            height: bigBoxesWidth
+        });
+        $(this).animate({
+            top: topPointer,
+            left: leftPointer
+        });
+
+        goToTop = true;
+
+        leftPointer += smallBoxesWidth * 2;
+
+    }
+
 
 });
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( jquery.fullPage.min.js )
-Rails.application.config.assets.precompile += %w( home-functions.js )
-Rails.application.config.assets.precompile += %w( tables-functions.js )
