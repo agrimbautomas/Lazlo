7d647c987fdc111ea2857c31ebc288f6c7d93891
Tomas Agrimbau <tomas@theamalgama.com>
2016-10-20 09:55:03 -0300

    Add Static Slider to home view

--- app/assets/javascripts/home-functions.js
+++ app/assets/javascripts/home-functions.js
@@ -2,63 +2,9 @@ var win;
 var elementsHeight;
 
 $(document).on('ready page:load', function (event) {
-
+    $('.bx-wrapper').bxSlider({
+        randomStart: true
+    });
 });
 
 
-
-
-// To hide navigation bar
-window.addEventListener("load", function () {
-    // Set a timeout...
-    setTimeout(function () {
-        // Hide the address bar!
-        window.scrollTo(0, 1);
-    }, 0);
-});
-
-
-/** New Home Functions **/
-$(document).ready(function () {
-
-
-    $('.contacto').click(function () {
-        $('.pre-contact').show();
-        $('.pre-contact').animate({
-            opacity: 1
-        });
-    });
-
-
-    $(document).keyup(function(e) {
-        if (e.keyCode == 27) { // escape key maps to keycode `27`
-            $('.pre-contact').animate({
-                opacity: 0
-            }, 300, function(){ $(this).hide() });
-        }
-    });
-
-
-
-
-    $('.pre-contact .contact input').focus(function () {
-        $(this).css('border', '3px dimgrey solid');
-    });
-
-
-    $('.pre-contact .contact input').blur(function () {
-        $(this).css('border', '2px #2ca0a0 solid;');
-    });
-
-
-
-    $('.pre-contact .contact textarea').focus(function () {
-        $(this).css('border', '3px dimgrey solid');
-    });
-
-
-    $('.pre-contact .contact textarea').blur(function () {
-        $(this).css('border', '2px #2ca0a0 solid;');
-    });
-
-});
