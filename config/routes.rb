303b9ad88e6b986157f702abda4620cdd1ecba61
Tomas Agrimbau <tomas@theamalgama.com>
2016-11-23 11:50:20 -0300

    Add devise facebook fix for confirmation emails

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  devise_for :users
+  devise_for :users, :controllers => {:omniauth_callbacks => 'omniauth_callbacks'}
 
 
 
