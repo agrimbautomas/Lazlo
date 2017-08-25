d618801a97017f411be6a675f3158e1067b5a28f
Tomas Agrimbau <agrimbautomas@gmail.com>
2017-08-25 16:21:01 -0300

    Add refactorization for pending purchases
    ref #34

--- app/interactors/purchases/pending_purchase.rb
+++ app/interactors/purchases/pending_purchase.rb
@@ -1,10 +1,26 @@
 class PendingPurchase < Purchase
 
-  def self.create_order
+  def cart_checkout
+	 order_products_list = OrderProductsList.create_from_list(@user.checkout_list)
+
+	 create_order(additional_info['title'], order_products_list)
+	 send_response_email
+  end
+
+  def single_checkout
+	 product = Product.find(product_data['id'])
+	 order_products_list = OrderProductsList.create_from_product(product)
+
+	 create_order(product_data['name'], order_products_list)
+	 send_response_email
+  end
+
+  def create_order title, products_list
 	 @order = Order.create(
 		  :user => @user,
-		  :order_products_list => order_products_list,
-		  :title => "[PENDING] #{title}",
+		  :order_products_list => products_list,
+		  :payment => products_list.total,
+		  :title => title,
 		  :payment_type => Order.payment_types[:mercado_pago],
 		  :order_status => OrderStatus.find_by_name('Encargado'),
 		  :detail => order_detail
@@ -14,7 +30,7 @@ class PendingPurchase < Purchase
 
   private
 
-  def self.send_response_email
+  def send_response_email
 	 BackgroundJob.run_block do
 		params = @purchase_params
 		params[:order] = @order
