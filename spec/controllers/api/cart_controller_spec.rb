e65ca32d71296f6b9a0c325b907a13b66a883ade
Tomas Agrimbau <tomas@theamalgama.com>
2017-09-09 17:26:06 -0300

    WIP Add controllers tests to add products

--- spec/controllers/api/cart_controller_spec.rb
+++ spec/controllers/api/cart_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CartController, type: :controller do
+  include ApplicationHelper
+
+  default_version 1
 
   let(:product) {create(:product)}
 
@@ -11,13 +14,18 @@ RSpec.describe Api::V1::CartController, type: :controller do
 		  post :add_product_to_cart, product_id: product.id
 		}
 
+		it 'should be redirected if not registered' do
+		  expect(response.status).to eq 302
+		end
+
 		it {
 		  expect(response.status).to eq 200
 		  expect(response.content_type).to eq('application/json')
 		}
 
 		it 'should respond success and update the cart' do
-		  expect(response.body).to match_response_schema('question-response')
+		  sign_in
+		  expect(response.body).to match_response_schema('product-cart-response')
 		end
 
 	 end
