1e93e96b38720c7cd8b9d496b9798abf58272175
Tomas Agrimbau <tomas@theamalgama.com>
2017-09-09 19:21:53 -0300

    Add test for cart controller

--- spec/controllers/api/cart_controller_spec.rb
+++ spec/controllers/api/cart_controller_spec.rb
@@ -5,29 +5,42 @@ RSpec.describe Api::V1::CartController, type: :controller do
 
   default_version 1
 
+  let(:user) {create(:user)}
   let(:product) {create(:product)}
+  let(:token) {double :acceptable? => true, :scopes => [:user]}
 
   describe 'CRUD products in user cart' do
 
-	 context 'POST product to user cart' do
-		before {
-		  post :add_product_to_cart, product_id: product.id
-		}
-
+	 context 'without Token' do
+		before {post :add_product_to_cart, product_id: product.id}
 		it 'should be redirected if not registered' do
 		  expect(response.status).to eq 302
 		end
+	 end
 
-		it {
-		  expect(response.status).to eq 200
-		  expect(response.content_type).to eq('application/json')
-		}
+	 context 'with Token' do
 
-		it 'should respond success and update the cart' do
-		  sign_in
-		  expect(response.body).to match_response_schema('product-cart-response')
+		before do
+		  allow(controller).to receive(:doorkeeper_token) {token}
+		  allow(controller).to receive(:current_user) {user}
 		end
 
+		context 'POST product to user cart' do
+		  before {
+			 post :add_product_to_cart, product_id: product.id
+		  }
+		  z 
+		  it {
+			 expect(response.status).to eq 200
+			 expect(response.content_type).to eq('application/json')
+		  }
+
+		  it 'should respond success and update the cart' do
+			 expect(response.body).to match_response_schema('product-cart-response')
+		  end
+
+		end
 	 end
   end
+
 end
