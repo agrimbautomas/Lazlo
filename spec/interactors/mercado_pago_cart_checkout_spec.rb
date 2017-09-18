5d67cb2a2c9b8e3b88e950f4ae05a3d2d98b3033
Tomas Agrimbau <tomas@theamalgama.com>
2017-09-18 10:46:20 -0300

    Add cart checkout tests
    ref #03

--- spec/interactors/mercado_pago_cart_checkout_spec.rb
+++ spec/interactors/mercado_pago_cart_checkout_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 RSpec.describe MercadoPagoCartCheckout do
 
-  let(:user) {create(:user_with_checkout_list)}
-  let!(:product) {create(:product)}
-
+  let(:user) { create(:user_with_checkout_list) }
+  let!(:product) { create(:product) }
 
   before {
 	 params = ActionController::Parameters.new
 	 params[:user] = user
-	 @single_checkout = MercadoPagoCartCheckout.(parameters: params, delegate: self)
+	 @cart_checkout = MercadoPagoCartCheckout.(parameters: params, delegate: self)
   }
 
   describe 'checkout method' do
 
 	 it 'matches the checkout link from mercado pago' do
-		expect(@single_checkout.checkout).to match('/sandbox.mercadopago.com/')
+		expect(@cart_checkout.checkout).to match('/sandbox.mercadopago.com/')
 	 end
 
   end
@@ -23,7 +22,8 @@ RSpec.describe MercadoPagoCartCheckout do
   describe 'purchase_title method' do
 
 	 it 'should return the purchase_title' do
-		expect(@single_checkout.send(:purchase_title)).to eq(product.name)
+		@cart_checkout.send(:items_to_json)
+		expect(@cart_checkout.send(:purchase_title)).to match(I18n.t('checkout_purchase_title'))
 	 end
 
   end
@@ -31,21 +31,26 @@ RSpec.describe MercadoPagoCartCheckout do
   describe 'back_urls_json method' do
 
 	 it 'should have the key pending in hash' do
-		expect(@single_checkout.send(:back_urls_json)).to have_key('pending')
+		expect(@cart_checkout.send(:back_urls_json)).to have_key('pending')
 	 end
 	 it 'should have the key success in hash' do
-		expect(@single_checkout.send(:back_urls_json)).to have_key('success')
+		expect(@cart_checkout.send(:back_urls_json)).to have_key('success')
 	 end
 	 it 'should have the key pending in hash' do
-		expect(@single_checkout.send(:back_urls_json)).to have_key('pending')
+		expect(@cart_checkout.send(:back_urls_json)).to have_key('pending')
 	 end
 
   end
 
   describe 'purchase_data method' do
+	 before { @cart_checkout.send(:items_to_json) }
 
-	 it 'should have the key product in hash' do
-		expect(@single_checkout.send(:purchase_data)).to have_key('product')
+	 it 'should have the key title in hash' do
+		expect(@cart_checkout.send(:purchase_data)).to have_key('title')
+	 end
+
+	 it 'should have the key product_rows in hash' do
+		expect(@cart_checkout.send(:purchase_data)).to have_key('product_rows')
 	 end
 
   end
@@ -53,32 +58,32 @@ RSpec.describe MercadoPagoCartCheckout do
   describe 'items_to_json method' do
 
 	 it 'should return an array' do
-		expect(@single_checkout.send(:items_to_json)).to be_a(Array)
+		expect(@cart_checkout.send(:items_to_json)).to be_a(Array)
 	 end
 	 it 'should have a hash of Product with data' do
-		expect(@single_checkout.send(:items_to_json).first).to be_a(Hash)
+		expect(@cart_checkout.send(:items_to_json).first).to be_a(Hash)
 	 end
 	 it 'should have the key id in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('id')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('id')
 	 end
 	 it 'should have the key title in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('title')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('title')
 	 end
 	 it 'should have the key description in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('description')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('description')
 	 end
 	 it 'should have the key quantity in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('quantity')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('quantity')
 	 end
 	 it 'sho<uld have the key unit_price in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('unit_price')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('unit_price')
 	 end
 	 it 'sho<uld have the key currency_id in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('currency_id')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('currency_id')
 	 end
 	 it 'sho<uld have the key picture_url in hash' do
-		expect(@single_checkout.send(:items_to_json).first).to have_key('picture_url')
+		expect(@cart_checkout.send(:items_to_json).first).to have_key('picture_url')
 	 end
-	 
+
   end
 end
