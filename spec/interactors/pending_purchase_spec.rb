f7fc5fa81777298dc398025f00ee15d3bb935140
Tomas Agrimbau <agrimbautomas@gmail.com>
2017-09-20 00:28:05 -0300

    Add pending purchase tests

--- spec/interactors/pending_purchase_spec.rb
+++ spec/interactors/pending_purchase_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe SuccessPurchase do
+RSpec.describe PendingPurchase do
   include MercadoPagoHelper
 
   let(:user) { create(:user_with_checkout_list) }
   let!(:product) { create(:product) }
 
   before {
+
 	 params = ActionController::Parameters.new
 	 params[:user] = user
 	 params[:product] = product
 	 params[:mercado_pago_params] = mercado_pago_params(user, product)
-
-	 @cart_checkout = SuccessPurchase.(params)
+	 
+	 @cart_checkout = PendingPurchase.(params)
   }
 
   describe 'single_checkout method' do
