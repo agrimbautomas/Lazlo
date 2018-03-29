ff12c30ffd52456336af2619757167ea388e69e5
Tomas Agrimbau <agrimbautomas@gmail.com>
2018-03-29 19:05:22 -0300

    Add staging config for deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,9 +2,9 @@ role :app, %w{deploy@lazlo.la}
 role :web, %w{deploy@lazlo.la}
 role :db,  %w{deploy@lazlo.la}
 
-set :deploy_to, '/srv/rails/macain-beta-web'
+set :deploy_to, '/srv/rails/lazlo-beta-web'
 
-server 'lazlo.la', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'beta.lazlo.la', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 set :rails_env, 'staging'
 set :branch, 'staging'
