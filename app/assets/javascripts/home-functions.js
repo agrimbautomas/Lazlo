1db733276fb53aa6cd8dec5895ae7cae8f26f9e1
Tomas Agrimbau <agrimbautomas@gmail.com>
2018-04-12 22:58:58 -0300

    Add scroll down header

--- app/assets/javascripts/home-functions.js
+++ app/assets/javascripts/home-functions.js
@@ -1,13 +1,60 @@
-var win;
-var elementsHeight;
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
 
-$( document ).ready(function() {
+$(document).ready(function () {
+    setUpHeaderToggleOnScroll();
+    setupSilderCarousel();
+});
 
+// Header Scroll functions
+function setUpHeaderToggleOnScroll() {
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+}
+
+$(window).scroll(function (event) {
+    didScroll = true;
+});
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    var navbarHeight = $('header').outerHeight();
+
+    if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if (st + $(window).height() < $(document).height())
+            $('header').removeClass('nav-up').addClass('nav-down');
+    }
+
+    lastScrollTop = st;
+}
+
+function setupSilderCarousel(){
     $('.bx-wrapper').bxSlider({
         randomStart: true,
         controls: false,
         mode: 'vertical'
     });
-});
-
-
+}
