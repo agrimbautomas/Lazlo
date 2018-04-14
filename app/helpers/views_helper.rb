1d2eb8f76d8440411aa5f20b8728c9002f5464e1
Tomas Agrimbau <agrimbautomas@gmail.com>
2018-04-14 18:29:18 -0300

    Add column sizes
    ref #55

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -4,4 +4,30 @@ module ViewsHelper
 	 text.split.map(&:capitalize)*' '
   end
 
+  def get_image_size column, size
+	 if column == 1
+		if size == 1
+		  image_size = :rectangular3
+		else
+		  image_size = :rectangular4
+		end
+
+	 elsif column == 2
+		if size == 1
+		  image_size = :rectangular4
+		else
+		  image_size = :rectangular5
+		end
+
+	 elsif column == 3
+		if size == 1
+		  image_size = :rectangular5
+		else
+		  image_size = :square1
+		end
+	 end
+
+	 image_size
+  end
+
 end
