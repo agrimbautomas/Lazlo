61719626f8f8a4d09e6e1117b02d69461baa47db
Tomas Agrimbau <tomas@amalgama.co>
2018-04-24 02:03:22 -0300

    Add slider arrows

--- app/assets/javascripts/products-functions.js
+++ app/assets/javascripts/products-functions.js
@@ -1,8 +1,8 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    setImagesGallery();
-    setToggleCartBtn();
-    setProductImagesSlider();
+	 setImagesGallery();
+	 setToggleCartBtn();
+	 setProductImagesSlider();
 });
 
 /**************************/
@@ -19,76 +19,75 @@ $( document ).ready(function() {
 /**************************/
 
 function setImagesGallery() {
-    $("#product-detail-zoom").elevateZoom({
-        gallery: 'product-images-gallery',
-        galleryActiveClass: 'active',
-        zoomType: "inner",
-        responsive: true,
-        cursor: "crosshair",
-        zoomWindowFadeIn: 500,
-        zoomWindowFadeOut: 750
-    });
+	 $("#product-detail-zoom").elevateZoom({
+		  gallery: 'product-images-gallery',
+		  galleryActiveClass: 'active',
+		  zoomType: "inner",
+		  responsive: true,
+		  cursor: "crosshair",
+		  zoomWindowFadeIn: 500,
+		  zoomWindowFadeOut: 750
+	 });
 }
 
 function setToggleCartBtn() {
-    $(".add-to-cart-btn").click(function (e) {
-        e.preventDefault();
+	 $(".add-to-cart-btn").click(function (e) {
+		  e.preventDefault();
 
-        if ($(this).hasClass('in_cart'))
-            removeFromCart($(this));
-        else
-            addToCart($(this));
+		  if ($(this).hasClass('in_cart'))
+				removeFromCart($(this));
+		  else
+				addToCart($(this));
 
-    })
+	 })
 
 }
 
 function addToCart($btn) {
-    var url = $btn.attr('href');
+	 var url = $btn.attr('href');
 
-    requestProductRow('POST', url, 1, addToCartCallback($btn));
+	 requestProductRow('POST', url, 1, addToCartCallback($btn));
 }
 
 
 function removeFromCart($btn) {
 
-    var url = $btn.data('rm-href');
+	 var url = $btn.data('rm-href');
 
-    $.ajax({
-        url: url,
-        context: document.body,
-        type: 'DELETE',
-        data: {
-            quantity: 1
-        }
-    }).done(function (data) {
-        if (data.response == 'success')
-            removeFromCartCallback($btn);
-    });
+	 $.ajax({
+		  url: url,
+		  context: document.body,
+		  type: 'DELETE',
+		  data: {
+				quantity: 1
+		  }
+	 }).done(function (data) {
+		  if (data.response == 'success')
+				removeFromCartCallback($btn);
+	 });
 
 }
 
-function addToCartCallback($btn){
-    incrementCartNumber();
-    addToCartStyles($btn);
+function addToCartCallback($btn) {
+	 incrementCartNumber();
+	 addToCartStyles($btn);
 }
 
-function removeFromCartCallback($btn){
-    decrementCartNumber();
-    removeFromCartStyles($btn);
+function removeFromCartCallback($btn) {
+	 decrementCartNumber();
+	 removeFromCartStyles($btn);
 }
 
 
 function addToCartStyles($btn) {
-    $btn.addClass('in_cart').addClass('green-button')
-        .removeClass('transparent-button').html('Guardado en el Carrito!');
+	 $btn.addClass('in_cart').addClass('green-button')
+		  .removeClass('transparent-button').html('Guardado en el Carrito!');
 }
 
 function removeFromCartStyles($btn) {
-    $btn.removeClass('in_cart').removeClass('green-button')
-        .addClass('transparent-button').html('Agregar al Carrito');
+	 $btn.removeClass('in_cart').removeClass('green-button')
+		  .addClass('transparent-button').html('Agregar al Carrito');
 }
-
 
 
 /**************************/
@@ -97,12 +96,18 @@ function removeFromCartStyles($btn) {
 /**************************/
 /**************************/
 
-function setProductImagesSlider(){
+function setProductImagesSlider() {
 	 $('.product-images-gallery').slick({
 		  infinite: true,
 		  slidesToShow: 3,
 		  slidesToScroll: 3,
-		  width: '32%'
+		  responsive: [{
+				breakpoint: 900,
+				settings: {
+					 slidesToShow: 2,
+					 slidesToScroll: 2
+				}
+		  }]
 	 });
 }
 
@@ -114,15 +119,15 @@ function setProductImagesSlider(){
 
 function requestProductRow(method, url, quantity, callback) {
 
-    $.ajax({
-        url: url,
-        context: document.body,
-        type: method,
-        data: {
-            quantity: quantity
-        }
-    }).done(function (data) {
-        if (data.response == 'success')
-            callback;
-    });
+	 $.ajax({
+		  url: url,
+		  context: document.body,
+		  type: method,
+		  data: {
+				quantity: quantity
+		  }
+	 }).done(function (data) {
+		  if (data.response == 'success')
+				callback;
+	 });
 }
