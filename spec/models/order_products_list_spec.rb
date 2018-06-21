e00a6df5f1a3cc2f418a6b23d229747c4653b695
Tomas Agrimbau <tomas@amalgama.co>
2018-06-21 11:36:51 -0300

    Add test for order product list
    ref #64

--- spec/models/order_products_list_spec.rb
+++ spec/models/order_products_list_spec.rb
@@ -12,34 +12,42 @@ require 'rails_helper'
 
 RSpec.describe OrderProductsList, type: :model do
 
-  it {should respond_to (:order)}
-  it {should belong_to (:order)}
+	it { should respond_to (:order) }
+	it { should belong_to (:order) }
 
-  it {should respond_to (:order_products_rows)}
-  it {should have_many (:order_products_rows)}
+	it { should respond_to (:order_products_rows) }
+	it { should have_many (:order_products_rows) }
 
-  let(:valid_order_products_list) {create(:order_products_list)}
-  let(:valid_product) {create(:product)}
+	let(:valid_order_products_list) { create(:order_products_list) }
+	let(:valid_product) { create(:product) }
 
-  describe '#create!' do
+	describe '#create!' do
 
-	 context 'with correct params' do
-		it {expect(valid_order_products_list).to be_valid}
-	 end
-  end
+		context 'with correct params' do
+			it { expect(valid_order_products_list).to be_valid }
+		end
+	end
 
-  describe 'instance methods' do
-	 context 'with products' do
-		it {
-		  expect(OrderProductsList.create_from_product(valid_product)).to be_a(OrderProductsList)
-		}
-	 end
+	describe 'instance methods' do
+		context 'with products' do
+			it {
+				expect(OrderProductsList.create_from_product(valid_product)).to be_a(OrderProductsList)
+			}
 
-	 context 'with products' do
-		it {
-		  expect(valid_order_products_list.formatted_total).to eq("$#{valid_order_products_list.total}")
-		}
-	 end
-  end
+
+			it 'should return the total price of all the products' do
+				expect(valid_order_products_list.total).to be > 0
+			end
+
+			it 'should return the total price of all the products as Integer or Float' do
+				expect(valid_order_products_list.total).to be_a(Integer).or(be_a(Float))
+			end
+
+			it 'should have at least one product row' do
+				expect(valid_order_products_list).to have_at_least(1).order_products_rows
+			end
+		end
+
+	end
 
 end
