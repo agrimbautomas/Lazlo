27b4e19898edaeb4ba16802455e0e1cfdb69c839
Tomas Agrimbau <tomas@amalgama.co>
2018-06-23 20:53:17 -0300

    Add testing response for chat endpoint

--- app/controllers/api/v1/chat_controller.rb
+++ app/controllers/api/v1/chat_controller.rb
@@ -4,10 +4,30 @@ class Api::V1::ChatController < Api::V1::ApiController
 	before_action :authenticate_user!, except: [:default_response]
 
 	def default_response
-		render_categories_response Category.all
+
+		if params[:queryResult].present?
+			action = params[:queryResult][:action]
+			case action
+				when 'get_categories'
+					get_categories
+
+				else
+					render_failed_response
+			end
+
+		end
+
 	end
 
 	private
+
+	def get_categories
+		response = Hash.new
+		response[:fulfillmentText] = 'Alguna respuesta para lazlo'
+		response[:source] = Settings.urls.base
+
+		render json: response
+	end
 
 	def render_categories_response(categories)
 		render_successful_response(
