486228b60e8a2ae2c35f56abd504cf6e17f218eb
Tomas Agrimbau <tomas@amalgama.co>
2018-06-23 17:42:36 -0300

    Add chatbot integration to website
    ref #64

--- app/assets/javascripts/site-functions.js
+++ app/assets/javascripts/site-functions.js
@@ -2,92 +2,134 @@ var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 
-
 var $contactForm;
+var $chatButton;
+var $chatWidget;
 
 
 jQuery(document).ready(function () {
-    setUpHeaderToggleOnScroll();
+	 setUpHeaderToggleOnScroll();
 
-    setupVars();
-    setupContactForm();
-    setupAlertButtons();
+	 setupVars();
+	 setupContactForm();
+	 setupAlertButtons();
+	 setupToggleChat();
 });
 
-// Header Scroll functions
+
+
+function setupVars() {
+	 $contactForm = $('.contact-form');
+	 $chatButton = $('.chat-button');
+	 $chatWidget = $('.chat-widget');
+}
+
+/*
+*
+* Chat functions
+*
+*/
+function setupToggleChat(){
+	 $chatButton.click(function(){
+	 	 if($chatWidget.hasClass('displayed'))
+			  hideChat();
+	 	 else
+	 	 	 displayChat()
+	 });
+}
+
+function displayChat(){
+
+	 $chatWidget.addClass('displayed');
+	 $('.display-on-chat').animate({
+		  opacity: 1
+	 }, 100);
+
+}
+
+function hideChat(){
+
+	 $('.display-on-chat').animate({
+		  opacity: 0
+	 }, 100, function(){
+		  $chatWidget.removeClass('displayed');
+	 });
+}
+
+/*
+*
+* Header Scroll functions
+*
+*/
 function setUpHeaderToggleOnScroll() {
 
-    $(window).scroll(function (event) {
-        didScroll = true;
-    });
+	 $(window).scroll(function (event) {
+		  didScroll = true;
+	 });
 
-    setInterval(function () {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
+	 setInterval(function () {
+		  if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+		  }
+	 }, 250);
 
 }
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
-    var navbarHeight = $('header').outerHeight();
+	 var st = $(this).scrollTop();
+	 var navbarHeight = $('header').outerHeight();
 
-    if (Math.abs(lastScrollTop - st) <= delta)
-        return;
+	 if (Math.abs(lastScrollTop - st) <= delta)
+		  return;
 
-    if (st > lastScrollTop && st > navbarHeight) {
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        if (st + $(window).height() < $(document).height())
-            $('header').removeClass('nav-up').addClass('nav-down');
-    }
+	 if (st > lastScrollTop && st > navbarHeight) {
+		  $('header').removeClass('nav-down').addClass('nav-up');
+	 } else {
+		  if (st + $(window).height() < $(document).height())
+				$('header').removeClass('nav-up').addClass('nav-down');
+	 }
 
-    lastScrollTop = st;
+	 lastScrollTop = st;
 }
 
-
-function setupVars() {
-    $contactForm = $('.contact-form')
-}
 
 function setupContactForm() {
-    $('.contact-form-button').each(function () {
-        $(this).click(function () {
-            showContactForm();
-        })
-    })
+	 $('.contact-form-button').each(function () {
+		  $(this).click(function () {
+				showContactForm();
+		  })
+	 })
 
-    $('.close-contact-form, .contact-form').click(function () {
-        hideContactForm();
-    })
+	 $('.close-contact-form, .contact-form').click(function () {
+		  hideContactForm();
+	 })
 
-    $('.contact-form-inner').click(function (e) {
-        e.stopPropagation();
-    })
+	 $('.contact-form-inner').click(function (e) {
+		  e.stopPropagation();
+	 })
 }
 
 
 function showContactForm() {
-    $contactForm.show().animate({
-        opacity: 1
-    }, 300);
+	 $contactForm.show().animate({
+		  opacity: 1
+	 }, 300);
 }
 
 function hideContactForm() {
-    $contactForm.animate({
-        opacity: 0
-    }, 300, function () {
-        $(this).hide()
-    });
+	 $contactForm.animate({
+		  opacity: 0
+	 }, 300, function () {
+		  $(this).hide()
+	 });
 }
 
 function setupAlertButtons() {
-    $('.notification-bar .close-notification-bar').each(function () {
-        $(this).click(function () {
-            $(this).parent().hide();
-        });
-    });
+	 $('.notification-bar .close-notification-bar').each(function () {
+		  $(this).click(function () {
+				$(this).parent().hide();
+		  });
+	 });
 }
 
