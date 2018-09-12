a2efe4a1056fa4f93296ecad0dcfc2124cf1b412
Tomas Agrimbau <tomas@amalgama.co>
2018-09-12 12:55:27 -0300

    Edit deploy production env

--- config/deploy/lazlo.rb
+++ config/deploy/lazlo.rb
@@ -4,7 +4,7 @@ role :db,  %w{deploy@lazlo.la}
 
 set :deploy_to, '/srv/rails/lazlo-beta-web'
 
-server 'lazlo.la', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'lazlo.la', user: 'deploy', roles: %w{web app}
 
-set :rails_env, 'staging'
+set :rails_env, 'production'
 set :branch, 'lazlo'
