cf5df7a153fd5d5fd733afdd85d7a50b24882389
Tomas Agrimbau <tomas@amalgama.co>
2018-11-01 14:39:12 -0300

    Force ssl over staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-	config.force_ssl = false
+	config.force_ssl = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
