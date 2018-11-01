97e0a62a5158cf8cbf7ee7b60774ee743962d4da
Tomas Agrimbau <tomas@amalgama.co>
2018-11-01 11:26:39 -0300

    Add tests for product size
    ref #82

--- spec/models/product_size_spec.rb
+++ spec/models/product_size_spec.rb
@@ -1,13 +1,42 @@
 require 'rails_helper'
+require 'contexts/for_models'
+
+RSpec.shared_examples 'creation is valid' do
+	it 'creation is valid' do
+		expect(
+			ProductSize.create(
+				name: name,
+				price: price,
+				product: product,
+			)
+		).to be_valid
+	end
+end
 
 RSpec.describe ProductSize, type: :model do
 
-  it { should respond_to (:name) }
-  it { should validate_presence_of (:name) }
-  it { should allow_value(false).for(:name) }
-  it { should validate_length_of(:name).is_at_most(255) }
+	it { should respond_to (:name) }
+	it { should validate_presence_of (:name) }
+	it { should allow_value(false).for(:name) }
+	it { should validate_length_of(:name).is_at_most(255) }
 
-  it { should respond_to(:product) }
-  it { should belong_to(:product) }
+	it { should respond_to (:price) }
+	it { should validate_presence_of (:price) }
+	it { should validate_numericality_of(:price).is_greater_than(0) }
+
+	it { should respond_to(:product) }
+	it { should belong_to(:product) }
+
+
+	describe '#create' do
+		include_context 'create product'
+		let( :price ) { 500.50 }
+		let( :name ) { '150cm x 200cm' }
+
+		context 'with all parameters' do
+			include_examples 'creation is valid'
+		end
+		
+	end
 
 end
--- spec/models/site_config_spec.rb
+++ spec/models/site_config_spec.rb
@@ -17,12 +17,15 @@ require 'rails_helper'
 
 RSpec.describe SiteConfig, type: :model do
 
-  it { should respond_to (:caption) }
-  it { should validate_presence_of (:caption) }
+	it { should respond_to (:caption) }
+	it { should validate_presence_of (:caption) }
 
-  it { should respond_to (:config_type) }
-  it { should validate_presence_of (:config_type) }
+	it { should respond_to (:config_type) }
+	it { should validate_presence_of (:config_type) }
 
-  it { should respond_to (:picture) }
-  it { should have_attached_file(:picture) }
+	it { should respond_to (:picture) }
+	it { should have_attached_file(:picture) }
+
+	it { expect define_enum_for(:category).with [:home_slide, :home_support, :home_promotion] }
+
 end
