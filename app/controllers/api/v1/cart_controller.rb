c716b36a84742c0a441407505320979d6449a693
Tomas Agrimbau <tomas@amalgama.co>
2018-11-05 12:32:38 -0300

    Add interactor to add product in controller
    ref #84

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -12,9 +12,7 @@ class Api::V1::CartController < Api::V1::ApiController
 	end
 
 	def add_product_to_cart
-		current_user.add_product_to_cart @product, @quantity
-		#AddProductToCart.with(user: current_user, product: @product, quantity: @quantity)
-
+		AddProductToCart.with(user: current_user, product: @product, quantity: @quantity)
 		render json: { :response => 'success' }
 	end
 
