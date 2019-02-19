2875ebc6c34f75bc0fccaf02f77f4ed3a42194b6
Tomas Agrimbau <tomas@amalgama.co>
2019-02-19 17:00:36 -0300

    Update rails version on capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,4 +5,4 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'key', 'certificates', 'challenge')
 
 set :keep_releases, 2
-set :rvm_ruby_version, '2.4.1'
+set :rvm_ruby_version, '2.5.1'
